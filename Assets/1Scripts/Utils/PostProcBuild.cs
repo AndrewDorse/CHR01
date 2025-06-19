013e46986625b9869c20f830905c2d86c804e563
Andrew <andrey.l@khelian.com>
2025-06-19 19:57:26 +0300

    19/6.1

--- Assets/1Scripts/Utils/PostProcBuild.cs
+++ Assets/1Scripts/Utils/PostProcBuild.cs
@@ -1,40 +1,41 @@
 
-//#if UNITY_EDITOR
-//using UnityEditor.Callbacks;
-//using UnityEditor.iOS.Xcode;
-//using UnityEditor;
-//using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEditor;
+using UnityEngine;
 
-//public static class PostProcBuild
-//{
-//    [PostProcessBuild]
-//    public static void AddToEntitlements(BuildTarget buildTarget, string buildPath)
-//    {
-//        if (buildTarget != BuildTarget.iOS) return;
+public static class PostProcBuild
+{
+    [PostProcessBuild]
+    public static void AddToEntitlements(BuildTarget buildTarget, string buildPath)
+    {
+        if (buildTarget != BuildTarget.iOS) return;
 
-//        // get project info
-//        string pbxPath = PBXProject.GetPBXProjectPath(buildPath);
-//        var proj = new PBXProject();
-//        proj.ReadFromFile(pbxPath);
-//        var guid = proj.GetUnityMainTargetGuid();
+        //// get project info
+        //string pbxPath = PBXProject.GetPBXProjectPath(buildPath);
+        //var proj = new PBXProject();
+        //proj.ReadFromFile(pbxPath);
+        //var guid = proj.GetUnityMainTargetGuid();
 
-//        // get entitlements path
-//        string[] idArray = Application.identifier.Split('.');
-//        var entitlementsPath = $"Unity-iPhone/{idArray[idArray.Length - 1]}.entitlements";
+        //// get entitlements path
+        //string[] idArray = Application.identifier.Split('.');
+        //var entitlementsPath = $"Unity-iPhone/{idArray[idArray.Length - 1]}.entitlements";
 
-//        // create capabilities manager
-//        var capManager = new ProjectCapabilityManager(pbxPath, entitlementsPath, null, guid);
+        //// create capabilities manager
+        //var capManager = new ProjectCapabilityManager(pbxPath, entitlementsPath, null, guid);
 
-//        // Add necessary capabilities
-//        capManager.AddPushNotifications(true);
+        //// Add necessary capabilities
+        //capManager.AddPushNotifications(true);
 
-//        // Write to file
-//        capManager.WriteToFile();
-//    }
+        //// Write to file
+        //capManager.WriteToFile();
+
+    }
 
 
 
 
-//}
+}
 
-//#endif
+#endif
