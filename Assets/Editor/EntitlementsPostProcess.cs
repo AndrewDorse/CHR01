92ccde2ea0c9ae765b4b89e7c7374d0467507fee
Andrew <andrey.l@khelian.com>
2025-06-21 14:27:29 +0300

    21/6

--- Assets/Editor/EntitlementsPostProcess.cs
+++ Assets/Editor/EntitlementsPostProcess.cs
@@ -10,45 +10,45 @@ using System.IO;
 [CreateAssetMenu]
 public class EntitlementsPostProcess : ScriptableObject
 {
-    public DefaultAsset m_entitlementsFile;
+    //public DefaultAsset m_entitlementsFile;
 
 
 
 
-    [PostProcessBuild]
-    public static void OnPostProcess(BuildTarget buildTarget, string buildPath)
-    {
-        if (buildTarget != BuildTarget.iOS)
-        {
-            return;
-        }
+    //[PostProcessBuild]
+    //public static void OnPostProcess(BuildTarget buildTarget, string buildPath)
+    //{
+    //    if (buildTarget != BuildTarget.iOS)
+    //    {
+    //        return;
+    //    }
 
 
-        var dummy = ScriptableObject.CreateInstance<EntitlementsPostProcess>();
-        var file = dummy.m_entitlementsFile;
-        ScriptableObject.DestroyImmediate(dummy);
-        if (file == null)
-        {
-            return;
-        }
+    //    var dummy = ScriptableObject.CreateInstance<EntitlementsPostProcess>();
+    //    var file = dummy.m_entitlementsFile;
+    //    ScriptableObject.DestroyImmediate(dummy);
+    //    if (file == null)
+    //    {
+    //        return;
+    //    }
 
-        var proj_path = PBXProject.GetPBXProjectPath(buildPath);
-        var proj = new PBXProject();
-        proj.ReadFromFile(proj_path);
+    //    var proj_path = PBXProject.GetPBXProjectPath(buildPath);
+    //    var proj = new PBXProject();
+    //    proj.ReadFromFile(proj_path);
 
-        // target_name = "Unity-iPhone"
-        var target_name = proj.GetUnityMainTargetGuid(); 
-        var target_guid = proj.TargetGuidByName(target_name);
-        var src = AssetDatabase.GetAssetPath(file);
-        var file_name = Path.GetFileName(src);
-        var dst = buildPath + "/" + target_name + "/" + file_name;
-        FileUtil.CopyFileOrDirectory(src, dst);
-        proj.AddFile(target_name + "/" + file_name, file_name);
-        proj.AddBuildProperty(target_guid, "CODE_SIGN_ENTITLEMENTS", target_name + "/" + file_name);
+    //    // target_name = "Unity-iPhone"
+    //    var target_name = proj.GetUnityMainTargetGuid(); 
+    //    var target_guid = proj.TargetGuidByName(target_name);
+    //    var src = AssetDatabase.GetAssetPath(file);
+    //    var file_name = Path.GetFileName(src);
+    //    var dst = buildPath + "/" + target_name + "/" + file_name;
+    //    FileUtil.CopyFileOrDirectory(src, dst);
+    //    proj.AddFile(target_name + "/" + file_name, file_name);
+    //    proj.AddBuildProperty(target_guid, "CODE_SIGN_ENTITLEMENTS", target_name + "/" + file_name);
 
 
-       // proj.AddFrameworkToProject(target_guid, "UserNotifications.framework", false);
+    //   // proj.AddFrameworkToProject(target_guid, "UserNotifications.framework", false);
 
-        proj.WriteToFile(proj_path);
-    }
+    //    proj.WriteToFile(proj_path);
+    //}
 }
